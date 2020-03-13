183c5efb959d187187f10f7c5fe450d830e90fe4
mykhailo_chervinko <Mykhailo_Chervinko@epam.com>
2020-03-13 21:47:24 +0200

    [COMMON] hot fixes

--- src/main/java/com/epam/telephonedirectory/controller/AdviceController.java
+++ src/main/java/com/epam/telephonedirectory/controller/AdviceController.java
@@ -3,6 +3,7 @@ package com.epam.telephonedirectory.controller;
 import com.epam.telephonedirectory.exception.NotFoundException;
 import com.epam.telephonedirectory.exception.PdfCreationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,9 +14,10 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class AdviceController {
 
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView notFoundException(NotFoundException e) {
-        log.error("Message = {}, Cause= {}", e.getMessage(), e.getCause());
+    @ExceptionHandler({NotFoundException.class, EmptyResultDataAccessException.class})
+    public ModelAndView notFoundException(Exception e) {
+        log(e);
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setStatus(HttpStatus.BAD_REQUEST);
         modelAndView.addObject("status", HttpStatus.BAD_REQUEST.getReasonPhrase());
@@ -27,7 +29,7 @@ public class AdviceController {
 
     @ExceptionHandler(PdfCreationException.class)
     public ModelAndView notFoundException(PdfCreationException e) {
-        log.error("Message = {}, Cause= {}", e.getMessage(), e.getCause());
+        log.info("Message = {}, Cause= {}", e.getMessage(), e.getCause());
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -36,5 +38,10 @@ public class AdviceController {
         modelAndView.addObject("cause", e.getCause());
         modelAndView.setViewName("pdfException");
         return modelAndView;
+    }
+
+
+    private void log(Exception e) {
+        log.error("Message = {}, Cause= {}", e.getMessage(), e.getCause());
     }
 }
--- src/main/java/com/epam/telephonedirectory/controller/UserController.java
+++ src/main/java/com/epam/telephonedirectory/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.epam.telephonedirectory.controller;
 
 import com.epam.telephonedirectory.domain.User;
-import com.epam.telephonedirectory.exception.PdfCreationException;
 import com.epam.telephonedirectory.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,42 +19,49 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.ByteArrayInputStream;
 import java.util.Collection;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/user")
-@Slf4j
 public class UserController {
     private UserService service;
 
     @PostMapping
     public User create(@RequestBody User user) {
-        log.info("create User: {}", user.toString());
+        log.debug("create User: {}", user.toString());
         return service.insert(user);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable(name = "id") Long id) {
-        log.info("delete User: id = '{}'", id);
+        log.debug("delete User: id = '{}'", id);
         service.delete(id);
         return ResponseEntity.accepted().build();
     }
 
+    @PatchMapping
+    public ResponseEntity<User> edit(@RequestBody User user) {
+        log.debug("edit User: {}", user.toString());
+        service.update(user);
+        return ResponseEntity.accepted().body(user);
+    }
+
     @GetMapping("/{id}")
     public User findById(@PathVariable(name = "id") Long id) {
-        log.info("Find User By Id  ='{}'", id);
+        log.debug("Find User By Id  ='{}'", id);
         return service.findById(id);
     }
 
     @GetMapping("/all")
     public Collection<User> findAll() {
-        log.info("findAll Users");
+        log.debug("findAll Users");
         return service.findAll();
     }
 
 
     @GetMapping(value = "/all/pdf")
     public ResponseEntity<?> downloadAllPDF() {
-        log.info("download All Users .PDF");
+        log.debug("download All Users .PDF");
 
         ByteArrayInputStream allInPDF = service.findAllInPDF();
 
